8cc42f7a41395a6afd1ca9419f0fa5c84c18ece7
vanhthu <voanhthu743@gmail.com>
2024-09-17 13:44:33 +0700

    Add project files.

---
+++ thuc_hanh_ctdl/C1_DisplayNums_Reference.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace thuc_hanh_ctdl
+{
+    internal class C1_DisplayNums_Reference
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuong trinh do thoi gian n so nguyen: ");
+            Console.Write("Nhap vao n = ");
+
+            long n = Convert.ToInt64(Console.ReadLine());
+            long[] nums = new long[n];
+            BuildArray(nums);
+            
+            Stopwatch timer = new Stopwatch();            
+
+            timer.Start();
+
+
+            DisplayNums(nums);
+            timer.Stop();
+
+            Console.WriteLine("Ket thuc thoi gian chay....");
+            long ms = timer.ElapsedMilliseconds;
+            Console.WriteLine("Ket qua do: T = {0} mili giay", ms.ToString());
+
+
+            Console.ReadKey();
+        }
+
+        static void BuildArray(long[] arr)
+        {
+            for (long i = 0; i < arr.Length; i++)
+                arr[i] = i;
+        }
+
+         static void DisplayNums(long[] arr)
+        {
+            for(long i = 0;i < arr.Length; i++)
+                Console.Write(arr[i]+ " ");
+        }
+    }
+}
