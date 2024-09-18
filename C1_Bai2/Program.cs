4fd3be180d8087f2bd6408c15dbfe1e88a76c038
vanhthu <voanhthu743@gmail.com>
2024-09-18 16:13:34 +0700

    Bai 2 - Doc du lieu tu file text thanh cong

--- C1_Bai2/Program.cs
+++ C1_Bai2/Program.cs
@@ -6,10 +6,15 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using System.IO;
 namespace C1_Bai2
 {
     internal class Program
     {
+        static void TinhThoiGianChoTungCachChay()
+        {
+
+        }
         static void NhapTuBanPhim()
         {            
             Console.Write("\n\nNhap vao so luong sinh vien: ");
@@ -93,15 +98,99 @@ namespace C1_Bai2
         }
 
 
+        static SinhVien[] NhapSinhVienKhoiTaoGiaTri()
+        {
+            Random random = new Random();
+            Console.Write("\n\nNhap vao so luong sinh vien: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            SinhVien[] dssv = new SinhVien[n];
+
+            for (int i = 0;i<n; i++)
+            {
+                string maSV = $"*Ma sinh vien: {i + 1}";
+                Console.WriteLine(maSV);
+
+                int mon1 = 50 + i % 50; // random từ 0-100
+                Console.WriteLine($"\t- Diem mon 1: {mon1}");
+
+                int mon2 = 60 + i % 40;
+                Console.WriteLine($"\t- Diem mon 2: {mon2}");
+
+
+                int mon3 = 70 + i % 30;
+                Console.WriteLine($"\t- Diem mon 3: {mon3}");
+
+
+                dssv[i] = new SinhVien(maSV, mon1, mon2, mon3);
+            }
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            double dtbCach1 = QLSV.DiemTrungBinhMon(dssv);
+            timer.Stop();
+
+
+            Console.WriteLine($"Diem trung binh theo cach 1: {dtbCach1}");
+            Console.WriteLine($"Thoi gian tinh diem trung binh theo cach 1: {timer.ElapsedMilliseconds} ms");
+
+            timer.Start();
+            double dtbCach2 = QLSV.DiemTrungBinhMonSV(dssv);
+            timer.Stop();
+
+            Console.WriteLine($"Diem trung binh theo cach 1: {dtbCach2}");
+            Console.WriteLine($"Thoi gian tinh diem trung binh theo cach 2: {timer.ElapsedMilliseconds} ms");
+            return dssv;
+        }
+
+
+        static SinhVien[] DocFileText(string filePath)
+        {
+            int numOfStudents = File.ReadAllLines(filePath).Length - 1;
+            SinhVien[] dssv = new SinhVien[numOfStudents];
+
+            StreamReader streamReader = new StreamReader(filePath);
+            string line;
+            string[] fields;
+
+            line = streamReader.ReadLine(); //header
+            int i = 0;         
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                fields = line.Split(',');
+                dssv[i] = new SinhVien();
+                dssv[i].masv = fields[0];
+                dssv[i].mon1 = Convert.ToInt32(fields[1]);
+                dssv[i].mon2 = Convert.ToInt32(fields[2]);                
+                dssv[i].mon3 = Convert.ToInt32(fields[3]);
+                dssv[i].DiemTrungBinh();
+                i++;
+            }
+            streamReader.Close();
+            double tongDiemTrungBinh = 0;
+            
+            foreach (SinhVien sv in dssv)
+            {
+                tongDiemTrungBinh += sv.DiemTrungBinh();
+            }
+            tongDiemTrungBinh /= numOfStudents;
+
+            Console.WriteLine($"Tong diem trung binh: {tongDiemTrungBinh}");
+            return dssv;
+        }
+
+
         static void Main(string[] args)
         {
             int choice;
             do
             {
-                Console.WriteLine("************** MENU ********************");
-                Console.WriteLine("*    1. Nhap tu ban phim                *");
-                Console.WriteLine("*    2. Nhap so luong ngau nhien        *");
-                Console.WriteLine("*****************************************");
+                Console.WriteLine("************** MENU **************************");
+                Console.WriteLine("*    1. Nhap tu ban phim                     *");
+                Console.WriteLine("*    2. Nhap so luong ngau nhien             *");
+                Console.WriteLine("*    3. Nhap sinh vien khoi tao gia tri      *");
+                Console.WriteLine("*    4. Doc du lieu tu file text             *");
+                Console.WriteLine("**********************************************");
 
                 Console.Write("Lua chon cua ban la: ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -109,6 +198,10 @@ namespace C1_Bai2
                 switch (choice) {
                     case 1: NhapTuBanPhim(); break;
                     case 2: NhapDuLieuNgauNhien(); break;
+                    case 3: NhapSinhVienKhoiTaoGiaTri(); break;
+                    case 4: 
+                        DocFileText("E:\\University\\the_last_one\\data_structure\\thuc_hanh_ctdl\\C1_Bai2\\sample_data\\sample_data1.txt"); 
+                        break;
                     case 0:
                         Console.WriteLine("Ban muon thoat khoi chuong trinh?");                        
                         break;
@@ -116,7 +209,7 @@ namespace C1_Bai2
                         Console.WriteLine("Du lieu khong hop le!");
                         break;
                 }
-                Console.WriteLine("Nhap phim bat ky de tiep tuc!");
+                Console.WriteLine("\nNhap phim bat ky de tiep tuc!");
                 Console.ReadKey();
                 Console.Clear();
             }
