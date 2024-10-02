bf3dddfce6c9c7c67f30d002a5ccb0d6b53186b8
vanhthu <voanhthu743@gmail.com>
2024-10-02 13:30:43 +0700

    Code phuong thức search thành công

---
+++ C2_Bai1_Search/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Bai1_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.Write("Nhap vao so luong phan tu cua mang: ");
+            int soLuong = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[soLuong];
+
+            ArrayInOut arrayInOut = new ArrayInOut();
+            ArrayInOut.NhapMang(arr, soLuong);
+            ArrayInOut.XuatMang(arr, soLuong);
+
+            Console.ReadKey();
+        }
+    }
+}
